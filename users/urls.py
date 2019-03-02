1f873d071f55d97130d4503e87cbfa0ca0fdc8f5
FAenX <kipronofb@gmail.com>
2019-03-03 02:49:52 +0300

    added name to users view

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from .views import UserListView, signup
 # andelans app url patterns
 
 urlpatterns = [
-    path('api/users/', UserListView.as_view()),
+    path('api/users/', UserListView.as_view(), name='users'),
     path('signup/', signup, name='signup'),
 ]
