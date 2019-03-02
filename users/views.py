38ca1445f7ce28e799fadb72940d71d68eea43df
FAenX <kipronofb@gmail.com>
2019-03-03 02:45:52 +0300

    fixed bug

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ from .forms import SignUpForm
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
